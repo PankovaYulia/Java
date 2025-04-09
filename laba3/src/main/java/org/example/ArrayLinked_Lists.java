c4f0e3e4b4c72d78b3740a0ea6031b37a175702b
юля <pankova020704@gmail.com>
2025-04-09 14:12:17 +0300

    джава доки

--- laba3/src/main/java/org/example/ArrayLinked_Lists.java
+++ laba3/src/main/java/org/example/ArrayLinked_Lists.java
@@ -3,11 +3,20 @@ package org.example;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Класс для тестирования производительности ArrayList и LinkedList.
+ * Сравнивает время выполнения основных операций для двух типов списков.
+ */
 public class ArrayLinked_Lists {
     private ArrayList<Integer> arrayList = new ArrayList<>();
     private LinkedList<Integer> linkedList = new LinkedList<>();
     private final int MAX_OPERATIONS = 16000;
 
+    /**
+     * Запускает тестирование производительности для операций:
+     * добавления, получения, проверки наличия, удаления и очистки элементов.
+     * Тестирование выполняется для разного количества операций (1000, 2000, 4000, ..., 16000).
+     */
     public final void testPerformance() {
         for (int i=1000; i<=MAX_OPERATIONS; i*=2){
             testAdd(i);
@@ -17,11 +26,24 @@ public class ArrayLinked_Lists {
             testClear(i);
         }
     }
+
+    /**
+     * Выводит результаты тестирования в консоль.
+     * @param operation Название тестируемой операции
+     * @param arrayListTime Время выполнения операции для ArrayList (в наносекундах на операцию)
+     * @param linkedListTime Время выполнения операции для LinkedList (в наносекундах на операцию)
+     * @param NUM_OPERATIONS Количество операций
+     */
     private void printResult(String operation, long arrayListTime, long linkedListTime, int NUM_OPERATIONS) {
         System.out.println("Operation\tCount\tArrayList Time (ns)\tLinkedList Time (ns)");
         System.out.println(operation + "\t" + NUM_OPERATIONS + "\t\t\t" + arrayListTime + "\t\t\t" + linkedListTime);
         System.out.println(" ");
     }
+
+    /**
+     * Тестирует производительность операции добавления элементов.
+     * @param NUM_OPERATIONS Количество операций добавления
+     */
     private void testAdd(int NUM_OPERATIONS) {
         long arrayListAddTime = measureTime(() -> {
             for (int i = 0; i < NUM_OPERATIONS; i++) {
@@ -37,6 +59,11 @@ public class ArrayLinked_Lists {
 
         printResult("Add     ", arrayListAddTime, linkedListAddTime, NUM_OPERATIONS);
     }
+
+    /**
+     * Тестирует производительность операции получения элементов по индексу.
+     * @param NUM_OPERATIONS Количество операций получения
+     */
     private void testGet(int NUM_OPERATIONS) {
         long arrayListGetTime = measureTime(() -> {
             for (int i = 0; i < NUM_OPERATIONS; i++) {
@@ -52,6 +79,11 @@ public class ArrayLinked_Lists {
 
         printResult("Get     ", arrayListGetTime, linkedListGetTime, NUM_OPERATIONS);
     }
+
+    /**
+     * Тестирует производительность операции проверки наличия элементов.
+     * @param NUM_OPERATIONS Количество операций проверки
+     */
     private void testContains(int NUM_OPERATIONS) {
         long arrayListContainsTime = measureTime(() -> {
             for (int i = 0; i < NUM_OPERATIONS; i++) {
@@ -67,6 +99,11 @@ public class ArrayLinked_Lists {
 
         printResult("Contains", arrayListContainsTime, linkedListContainsTime, NUM_OPERATIONS);
     }
+
+    /**
+     * Тестирует производительность операции удаления элементов из начала списка.
+     * @param NUM_OPERATIONS Количество операций удаления
+     */
     private void testDelete(int NUM_OPERATIONS) {
         long arrayListDeleteTime = measureTime(() -> {
             for (int i = 0; i < NUM_OPERATIONS; i++) {
@@ -82,6 +119,12 @@ public class ArrayLinked_Lists {
 
         printResult("Delete  ", arrayListDeleteTime, linkedListDeleteTime, NUM_OPERATIONS);
     }
+
+    /**
+     * Тестирует производительность операции очистки списка.
+     * Перед тестированием заполняет списки указанным количеством элементов.
+     * @param NUM_OPERATIONS Количество элементов для добавления перед очисткой
+     */
     private void testClear(int NUM_OPERATIONS) {
         for (int i = 0; i < NUM_OPERATIONS; i++) {
             arrayList.add(i);
@@ -98,6 +141,13 @@ public class ArrayLinked_Lists {
 
         printResult("Clear   ", arrayListClearTime, linkedListClearTime, NUM_OPERATIONS);
     }
+
+    /**
+     * Измеряет среднее время выполнения одной операции.
+     * @param task Задача, время выполнения которой нужно измерить
+     * @param NUM_OPERATIONS Количество операций в задаче
+     * @return Среднее время выполнения одной операции в наносекундах
+     */
     private long measureTime(Runnable task, int NUM_OPERATIONS) {
         long startTime = System.nanoTime();
         task.run();
