5d084bc5149f7fdae003495c5d11d137af895f7c
юля <pankova020704@gmail.com>
2025-04-09 14:01:39 +0300

    тест получения элементов

--- laba3/src/main/java/org/example/ArrayLinked_Lists.java
+++ laba3/src/main/java/org/example/ArrayLinked_Lists.java
@@ -11,6 +11,8 @@ public class ArrayLinked_Lists {
     public final void testPerformance() {
         for (int i=1000; i<=MAX_OPERATIONS; i*=2){
             testAdd(i);
+            testGet(i);
+            testContains(i);
         }
     }
     private void printResult(String operation, long arrayListTime, long linkedListTime, int NUM_OPERATIONS) {
@@ -32,6 +34,36 @@ public class ArrayLinked_Lists {
         }, NUM_OPERATIONS);
 
         printResult("Add     ", arrayListAddTime, linkedListAddTime, NUM_OPERATIONS);
+    }
+    private void testGet(int NUM_OPERATIONS) {
+        long arrayListGetTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                arrayList.get(i);
+            }
+        }, NUM_OPERATIONS);
+
+        long linkedListGetTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                linkedList.get(i);
+            }
+        }, NUM_OPERATIONS);
+
+        printResult("Get     ", arrayListGetTime, linkedListGetTime, NUM_OPERATIONS);
+    }
+    private void testContains(int NUM_OPERATIONS) {
+        long arrayListContainsTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                arrayList.contains(i);
+            }
+        }, NUM_OPERATIONS);
+
+        long linkedListContainsTime = measureTime(() -> {
+            for (int i = 0; i < NUM_OPERATIONS; i++) {
+                linkedList.contains(i);
+            }
+        }, NUM_OPERATIONS);
+
+        printResult("Contains", arrayListContainsTime, linkedListContainsTime, NUM_OPERATIONS);
     }
     private long measureTime(Runnable task, int NUM_OPERATIONS) {
         long startTime = System.nanoTime();
