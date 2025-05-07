25d45730ad8f3529753212deb4a221c0fea93e83
юля <pankova020704@gmail.com>
2025-05-07 19:05:37 +0300

    Инжектор и финальная настройка

--- laba5/src/main/java/Main.java
+++ laba5/src/main/java/Main.java
@@ -1,23 +1,7 @@
-import interfaces.SomeInterface;
-import interfaces.SomeOtherInterface;
-import implementations.SomeImpl;
-import implementations.OtherImpl;
-import implementations.SODoer;
-
 public class Main {
     public static void main(String[] args) {
-        // Тестируем реализации вручную
-        SomeInterface impl1 = new SomeImpl();
-        SomeInterface impl2 = new OtherImpl();
-        SomeOtherInterface soImpl = new SODoer();
-
-        System.out.println("Testing implementations:");
-        impl1.doSomething();  // A
-        impl2.doSomething();  // B
-        soImpl.doSomeOther(); // C
-
-        // Первая версия SomeBean без DI
-        SomeBean bean = new SomeBean();
-        System.out.println("Warning: SomeBean not properly initialized yet");
+        SomeBean someBean = new SomeBean();
+        someBean = new Injector().inject(someBean);
+        someBean.foo();
     }
 }
