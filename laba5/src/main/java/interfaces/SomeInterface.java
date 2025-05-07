64b4a79a26cc93d03fc0647d7dc3e00ceddbe175
юля <pankova020704@gmail.com>
2025-05-07 18:57:45 +0300

    Реализации интерфейсов и класс SomeBean

--- laba5/src/main/java/interfaces/SomeInterface.java
+++ laba5/src/main/java/interfaces/SomeInterface.java
@@ -1,4 +1,4 @@
-package org.example.interfaces;
+package interfaces;
 
 public interface SomeInterface {
     public void doSomething();
--- laba5/src/main/java/interfaces/SomeOtherInterface.java
+++ laba5/src/main/java/interfaces/SomeOtherInterface.java
@@ -1,4 +1,4 @@
-package org.example.interfaces;
+package interfaces;
 
 public interface SomeOtherInterface {
     public void doSomeOther();
